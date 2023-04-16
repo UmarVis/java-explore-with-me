c4c54b7137ecaddd27fcc68f8b28d91ef0e3a1f2
umarvis <umar_9292@mail.ru>
2023-04-17 00:18:08 +0300

    on rev 1

--- stat-server/src/main/java/ru/practicum/controller/StatController.java
+++ stat-server/src/main/java/ru/practicum/controller/StatController.java
@@ -22,16 +22,16 @@ public class StatController {
 
     @PostMapping("/hit")
     @ResponseStatus(HttpStatus.CREATED)
-    public void addHit (@RequestBody DtoHitIn dtoHitIn) {
+    public void addHit(@RequestBody DtoHitIn dtoHitIn) {
         log.info("Информация сохранена {}", dtoHitIn);
         statService.addHit(dtoHitIn);
     }
 
     @GetMapping("/stats")
-    public List<DtoStatOut> getStats (@RequestParam @DateTimeFormat(pattern = DATA_TIME_FORMAT) LocalDateTime start,
-                                @RequestParam @DateTimeFormat(pattern = DATA_TIME_FORMAT) LocalDateTime end,
-                                @RequestParam(required = false) List<String> uris,
-                                @RequestParam(defaultValue = "false") Boolean unique) {
+    public List<DtoStatOut> getStats(@RequestParam @DateTimeFormat(pattern = DATA_TIME_FORMAT) LocalDateTime start,
+                                     @RequestParam @DateTimeFormat(pattern = DATA_TIME_FORMAT) LocalDateTime end,
+                                     @RequestParam(required = false) List<String> uris,
+                                     @RequestParam(defaultValue = "false") Boolean unique) {
         log.info("Получение статистики: start {}, end {}", start, end);
         return statService.getStats(start, end, uris, unique);
     }
